19a782baa45162ce565f35845b0a2ff9f1866b83
Jennifer Okeke <aestheticjenny04@gmail.com>
2025-02-24 20:33:21 +0000

    initialize project

---
+++ moviemuse-backend/moviemuse/src/main/java/com/example/moviemuse/MoviemuseApplication.java
@@ -0,0 +1,13 @@
+package com.example.moviemuse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviemuseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviemuseApplication.class, args);
+	}
+
+}
