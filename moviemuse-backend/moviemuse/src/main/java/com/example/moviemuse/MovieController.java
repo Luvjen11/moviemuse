72cc05b55e7496c2745b16fe5cb3b9e01e30625e
Jennifer Okeke <aestheticjenny04@gmail.com>
2025-02-24 21:22:44 +0000

    create movie controller with get mapping to find all movies

---
+++ moviemuse-backend/moviemuse/src/main/java/com/example/moviemuse/MovieController.java
@@ -0,0 +1,25 @@
+package com.example.moviemuse;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/moviemuse")
+public class MovieController {
+    
+    @Autowired
+    private MovieService movieService;
+
+    @GetMapping
+    public ResponseEntity<List<Movie>> getAllMovies() {
+        List<Movie> movies = movieService.findAllMovies();
+        System.out.println(movies);
+        return new ResponseEntity<List<Movie>>(movieService.findAllMovies(), HttpStatus.OK);
+    }
+}
