772ba4acf60ea205e982e10509ca8bb9b3597e75
Jennifer Okeke <aestheticjenny04@gmail.com>
2025-02-24 21:04:30 +0000

    create movie repository

---
+++ moviemuse-backend/moviemuse/src/main/java/com/example/moviemuse/MovieRepository.java
@@ -0,0 +1,9 @@
+package com.example.moviemuse;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MovieRepository extends JpaRepository<Movie, Long>{
+
+    
+    
+}
