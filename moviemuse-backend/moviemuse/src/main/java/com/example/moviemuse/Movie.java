d7df8675ce4ca45abf103821a5390e07045d62ae
Jennifer Okeke <aestheticjenny04@gmail.com>
2025-02-24 21:02:29 +0000

    create movie entity

---
+++ moviemuse-backend/moviemuse/src/main/java/com/example/moviemuse/Movie.java
@@ -0,0 +1,39 @@
+package com.example.moviemuse;
+
+import java.util.List;
+
+import org.springframework.data.annotation.Id;
+
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "movies")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Movie {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private int episodes;
+    private String poster;
+    @ElementCollection
+    private List<String> category;
+    @ElementCollection
+    private List<String> genres;
+
+    @OneToMany(mappedBy = "movie")
+    private List<Review> reviews;
+
+}
---
+++ moviemuse-backend/moviemuse/src/main/java/com/example/moviemuse/Review.java
@@ -0,0 +1,5 @@
+package com.example.moviemuse;
+
+public class Review {
+    
+}
