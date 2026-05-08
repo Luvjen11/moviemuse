bffa410e0b20fe964c075c47bd1f86787276e776
Jennifer Okeke <jennifer@digitalexhibitiongroup.com>
2026-05-08 09:20:03 +0100

    create the recommender.py file and query 5000 animes with image to use as data

--- notebook/anilist.py
+++ notebook/anilist.py
@@ -1,6 +1,6 @@
 import requests
 import json
-import time 
+import time
 
 url = "https://graphql.anilist.co"
 
@@ -15,37 +15,51 @@ query ($page: Int, $perPage: Int) {
       }
       genres
       description
+      coverImage {
+        large
+      }
+      averageScore
     }
   }
 }
 """
-# save 5000 animes 
+
 all_anime = []
 perPage = 50
 pages_needed = 100
 
-for page in range(1, pages_needed + 1): 
+for page in range(1, pages_needed + 1):
     variables = {
         "page": page,
-        "perPage" : perPage
+        "perPage": perPage
     }
 
     response = requests.post(
-        url,
-        json={
-            "query": query,
-            "variables": variables
-        }
+    url,
+    json={
+        "query": query,
+        "variables": variables
+    }
     )
 
+    print("status:", response.status_code)
+    print("text:", response.text[:1000])  # first 1000 chars only
+
     data = response.json()
+
+    if "data" not in data:
+        print(f"Problem on page {page}:")
+        print(data)
+        break
+
     media = data["data"]["Page"]["media"]
     all_anime.extend(media)
+
     print(f"Fetched page {page}, total so far: {len(all_anime)}")
     time.sleep(0.5)
-  
 
-with open("anilist_5000.json", "w", encoding="utf-8") as f:
+with open("anilist_5000_with_image.json", "w", encoding="utf-8") as f:
     json.dump(all_anime, f, ensure_ascii=False, indent=2)
 
-print(f"Saved {len(all_anime)} anime to anilist_1000.json")
+print(f"Saved {len(all_anime)} anime to anilist_5000_with_image.json")
+
